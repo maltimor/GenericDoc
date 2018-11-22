4c20d7e1e1a4141346e287918650cb7de0ff16ab
maltimor <maltimor@gmail.com>
2018-11-22 23:49:08 +0100

    Delete UsuarioServiceDao.java

--- src/main/java/es/maltimor/genericDoc/dao/UsuarioServiceDao.java
+++
@@ -1,7 +0,0 @@
-package es.maltimor.genericDoc.dao;
-
-public interface UsuarioServiceDao {
-	public String getDNI(String login);
-	public String getNombreCompletoPersona(String login);
-	public String getEntorno();
-}
